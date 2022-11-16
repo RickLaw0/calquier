95ebfdce8ebce1d32caa97c2715e41d20b707a0e
RickLaw0 <116848417+RickLaw0@users.noreply.github.com>
2022-11-16 16:16:45 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,8 @@
+// $("#menu").click(function () {
+//   $("#contenedormenu").slideToggle();
+// });
+
+// Funcion para el menu responsive
+$("#menu").click(function () {
+    $("caja_menu").toggleClass("abrirmenu");
+  });
